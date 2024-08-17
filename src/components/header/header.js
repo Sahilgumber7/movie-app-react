c2a7f5912f522a2a0ec2fed58f736a28c59dd900
sahilgumber7 <sahilgumber13418@gmail.com>
2024-08-18 03:24:00 +0530

    first-commit

---
+++ src/components/header/header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header__icon" src="https://www.logoai.com/uploads/output/2023/11/14/4820ffe418fb3f64053fca0b56bd4ee3.jpg" /></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
